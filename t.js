89ac6c503c8dabd5ac4bb77e1b3c3949b377ebec
Sakshimalde <sakshimalde1824@gmail.com>
2024-10-08 14:09:39 +0530

    commit

---
+++ t.js
@@ -0,0 +1,2 @@
+document.querySelector(".login").addEventListener("click",()=>{document.getElementById("login-form-container").style.display="block";})
+document.querySelector(".register").addEventListener("click",()=>{document.getElementById("register-form-container").style.display="block";})
