34f9df275464c157277ae76685cc3021e8b3f6b6
Sakshimalde <sakshimalde1824@gmail.com>
2024-10-14 17:18:46 +0530

    commit

--- home.js
+++ home.js
@@ -1,2 +1,16 @@
 document.querySelector(".login").addEventListener("click",()=>{document.getElementById("login-form-container").style.display="block";})
 document.querySelector(".register").addEventListener("click",()=>{document.getElementById("register-form-container").style.display="block";})
+
+const carousel = document.querySelector('.carousel');
+const carouselInner = document.querySelector('.carousel-inner');
+const carouselItems = document.querySelectorAll('.carousel-item');
+
+let currentSlide = 0;
+
+function fadeNextSlide() {
+  carouselItems[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % carouselItems.length;
+  carouselItems[currentSlide].classList.add('active');
+}
+
+setInterval(fadeNextSlide, 2000); // adjust the interval as needed
