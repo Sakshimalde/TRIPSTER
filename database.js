56105c8561ccce09a5899e46e179fe1533fcb02b
Sakshimalde <sakshimalde1824@gmail.com>
2024-10-31 19:40:34 +0530

    msg

---
+++ database.js
@@ -0,0 +1,34 @@
+const express=require("express");
+const app=express();
+const mongoose=require('mongoose');
+const db_link='mongodb+srv://sakshimalde:7a4u5g14JdDJ3hYm@cluster0.hyyfr.mongodb.net/';
+mongoose.connect(db_link)
+.then(function(db){
+    console.log(db);
+    console.log('db connected');
+})
+.catch(function(err){
+    console.log(err);
+});
+
+const userSchema=mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            required:true,
+            minLength:8
+        },
+    }
+);
+
+const user=mongoose.model('User',userSchema)
+module.exports = user;
