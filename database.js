d1fddedaa3e316ff244f3e2ca46ef6826b9791a0
Sakshimalde <sakshimalde1824@gmail.com>
2024-11-17 16:44:30 +0530

    updates

--- database.js
+++ database.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const app=express();
 const mongoose=require('mongoose');
+const emailvalidator=require('email-validator');
 const db_link='mongodb+srv://sakshimalde:7a4u5g14JdDJ3hYm@cluster0.hyyfr.mongodb.net/';
 mongoose.connect(db_link)
 .then(function(db){
     console.log(db);
     console.log('db connected');
+    maxPoolSize: 500;
 })
 .catch(function(err){
     console.log(err);
@@ -20,15 +22,30 @@ const userSchema=mongoose.Schema(
         email:{
             type:String,
             required:true,
-            unique:true
+            unique:true,
+            validate:function(){
+                return emailvalidator.validate(this.email);
+            }
         },
         password:{
             type:String,
             required:true,
             minLength:8
         },
+        confirmPassword:{
+            type:String,
+            required:true,
+            minLength:8,
+            validate:function(){
+                return this.confirmPassword==this.password
+            }
+        },
     }
 );
+
+userSchema.pre('save',function(){
+    this.confirmPassword=undefined;
+})
 
 const user=mongoose.model('User',userSchema)
 module.exports = user;
--- profile.js
+++
@@ -1,9 +0,0 @@
-// profile.js
-document.getElementById("edit-button").addEventListener("click", function () {
-    const form = document.getElementById("edit-profile-form");
-    if (form.style.display === "none") {
-        form.style.display = "block";
-    } else {
-        form.style.display = "none";
-    }
-});
